074e221501f4533022cacbc07644eec7b8f2db56
Juhwa Hwang <zerothhan@gmail.com>
2022-08-24 10:47:34 +0900

    7. 연락처 추가

--- src/component/ContactForm.js
+++ src/component/ContactForm.js
@@ -1,13 +1,20 @@
 import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
+import { useDispatch } from "react-redux";
 
 const ContactForm = () => {
   const [name, setName] = useState("");
   const [phoneNumber, setPhoneNumber] = useState(0);
+  const dispatch = useDispatch();
+
+  const addContact = (event) => {
+    event.preventDefault();
+    dispatch({ type: "ADD_CONTACT", payload: { name, phoneNumber } });
+  };
 
   return (
     <div>
-      <Form>
+      <Form onSubmit={addContact}>
         <Form.Group className="mb-3" controlId="formName">
           <Form.Label>이름</Form.Label>
           <Form.Control
